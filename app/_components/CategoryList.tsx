ff6b7ec822320c0042ac75d72cd0e1ba72138586
marleypm16 <yarlempm16@gmail.com>
2024-05-02 10:46:47 -0300

    feat : initial CategoryList.tsx commit

---
+++ app/_components/CategoryList.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Category} from "@prisma/client";
+import {db} from "@/app/_lib/prisma";
+import CategoryItem from "@/app/_components/CategoryItem";
+const CategoryList = async () => {
+    const categories = await db.category.findMany();
+    return (
+            <div className="flex gap-3">
+                {categories.map((category) => (
+                    <CategoryItem key={category.id} category={category}/>
+                ))}
+            </div>
+
+    );
+};
+
+export default CategoryList;
